a54f49d398a7da59ac41e0ca8ed57f542a750a75
ReachinNickChan <nigel.chan@reachinasia.com>
2025-06-27 23:28:36 +1000

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -46,7 +46,7 @@ const Header = () => {
               <img 
                 src={reachinLogo} 
                 alt="Reachin - Business Development Outsourcing" 
-                className="h-14 w-auto"
+                className="h-15 w-auto"
               />
             </Link>
           </div>
